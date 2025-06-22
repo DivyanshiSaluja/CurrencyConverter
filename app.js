7d8bf60534365b1af19362ea31d95e388d0135d9
DivyanshiSaluja <divyanshi.saluja2024@vitstudent.ac.in>
2025-06-22 15:15:19 +0530

    Add files via upload
    Add initial front-end files

---
+++ app.js
@@ -0,0 +1,15 @@
+const BASE_URL = "https://2024-03-06.currency-api.pages.dev/v1/currencies/eur.json";
+const dropdowns =document.querySelectorAll(".dropdown select");
+
+// for (code in countryList) {
+//     console.log(code);
+// }
+
+// for (let select of dropdowns ){
+//     for (currCode in countryList) {
+//         let newOption = document.querySelector("option");
+//         newOption.innerText = currCode;
+//         newOption.value = currCode;
+//         select.append(newOption);
+//     }
+// }
